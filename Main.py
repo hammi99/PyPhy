726cf8c3120b4369b27976a8c11e2fc918507f39
EeshaArif <eeshaarif@gmail.com>
2020-05-25 06:07:11 +0500

    cleaned output-display

--- Main.py
+++ Main.py
@@ -2,30 +2,28 @@ from Classes import *
 from Functions import *
 from math import *
 
-no_of_bodies = int(input('No of bodies:'))
+no_of_bodies = int(input('No of bodies: '))
 
-bodies=[]
+bodies = []
 
 for _ in range(no_of_bodies):
+    n = _ + 1
+    print("\nInputing values of Body {}".format(n))
+    mass = int(input('mass_{}:\t\t'.format(n)))
+    radius = int(input('radius_{}:\t'.format(n)))
 
-    mass = int(input('mass'))
-    radius =int(input('radius'))
+    position_mag = int(input('pos mag_{}:\t'.format(n)))
+    position_angle = int(input('pos angle_{}:\t'.format(n)))
+    position = Vector(position_mag, position_angle)
 
+    velocity_mag = int(input('vel mag_{}:\t'.format(n)))
+    velocity_angle = int(input('vel angl_{}:\t'.format(n)))
+    velocity = Vector(velocity_mag, velocity_angle)
 
-    position_mag = int(input('pos mag'))
-    position_angle = int(input('pos angle'))
-    position = Vector(position_mag ,position_angle)
-
-    velocity_mag = int(input('vel mag'))
-    velocity_angle = int(input('vel angl'))
-    velocity = Vector(velocity_mag ,velocity_angle)
-
-    acceleration_mag = int(input('acc mag'))
-    acceleration_angle = int(input('acc angl'))
-    acceleration = Vector(acceleration_mag ,acceleration_angle)
-
-
-    bodies.append(Bodies(mass ,radius ,position ,velocity ,acceleration))
+    acceleration_mag = int(input('acc mag_{}:\t'.format(n)))
+    acceleration_angle = int(input('acc angl_{}:\t'.format(n)))
+    acceleration = Vector(acceleration_mag, acceleration_angle)
+    bodies.append(Bodies(mass, radius, position, velocity, acceleration))
 
 while True:
     for body in bodies:
@@ -34,11 +32,5 @@ while True:
         body.vel.Update(body.acc)
         #body.acc.Update(Vector(Force/body.mas ,))
 
-    #for body1 in bodies:
+    # for body1 in bodies:
      #   for body2 in bodies:
-
-    
-
-
-
-
