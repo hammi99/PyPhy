a972ab16296af43df0b3a29a88c5a3987be60091
hammad1015 <45166466+hammad1015@users.noreply.github.com>
2020-05-27 22:07:44 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,34 +2,39 @@ import pyglet
 import physics
 import numpy as np
 
+x = 800
+y = 800 
 b = 100
 d = 2
 G = 5
-dt = 1/120
+dt = 1/200
+bounds = [x, y]
 
 sim = physics.Simulation(
-    np.random.random([b]) * 100
-    ,np.random.random([b, d]) * 100 + 400
-    ,np.zeros([b, d])
-    #,np.random.random([m, 2])
-    ,G
+     masses     =  np.random.random([b]) * 100
+    ,positions  =  np.random.random([b, d]) * 100 + 350
+    ,velocities = (np.random.random([b, d]) - 0.5) * 50
+    ,G          =  G
+    ,bounds     = bounds
+    ,radii      = 0
 )
 
-window = pyglet.window.Window(800, 800)
+window = pyglet.window.Window(x, y)
 p = sim.r.ravel()
 n = sim.r.shape[0]
+
 
 def update(dt):
     global sim
     sim.step(dt)
+    sim.check_bounds()
+    fps = pyglet.clock.get_fps()
+    print(fps)
 
-
-@window.event
-def on_key_press(symbol, modifiers):
-    print('A key was pressed')
 
 @window.event
 def on_draw():
+
     window.clear()
     pyglet.graphics.draw(n, pyglet.gl.GL_POINTS
         ,('v2f', p)
